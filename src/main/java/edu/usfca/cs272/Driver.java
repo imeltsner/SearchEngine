1b0238c1136d5e251f43e31d907cb991a222b567
isaac meltsner <itmeltsner@dons.usfca.edu>
2023-09-03 20:13:53 -0700

    first edits

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -1,8 +1,9 @@
 package edu.usfca.cs272;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 /**
  * Class responsible for running this project based on the provided command-line
@@ -13,6 +14,12 @@ import java.util.Arrays;
  * @version Fall 2023
  */
 public class Driver {
+	public static ArrayList<String> processFile(Path input) throws IOException {
+		ArrayList<String> stems = FileStemmer.listStems(input);
+		return stems;
+	}
+
+
 	/**
 	 * Initializes the classes necessary based on the provided command-line
 	 * arguments. This includes (but is not limited to) how to build or search an
@@ -21,32 +28,14 @@ public class Driver {
 	 * @param args flag/value pairs used to start this program
 	 */
 	public static void main(String[] args) {
-		// store initial start time
-		Instant start = Instant.now();
-
-		// TODO Fill in and modify as needed
-		System.out.println(Arrays.toString(args));
-
-		// calculate time elapsed and output
-		long elapsed = Duration.between(start, Instant.now()).toMillis();
-		double seconds = (double) elapsed / Duration.ofSeconds(1).toMillis();
-		System.out.printf("Elapsed: %f seconds%n", seconds);
+		Path input = Path.of("/Users/isaacmeltsner/Desktop/CS/CS272-C/SearchEngine/project-tests/input/text/simple/hello.txt");
+		if (!Files.isDirectory(input)) {
+			try {
+				ArrayList<String> stems = processFile(input);
+	
+			} catch (IOException e) {
+				System.out.println("File not found");
+			}
+		}
 	}
-
-	/*
-	 * Generally, "Driver" classes are responsible for setting up and calling other
-	 * classes, usually from a main() method that parses command-line parameters.
-	 * Generalized reusable code are usually placed outside of the Driver class.
-	 * They are sometimes called "Main" classes too, since they usually include the
-	 * main() method.
-	 *
-	 * If the driver were only responsible for a single class, we use that class
-	 * name. For example, "TaxiDriver" is what we would name a driver class that
-	 * just sets up and calls the "Taxi" class.
-	 *
-	 * The starter code (calculating elapsed time) is not necessary. It can be
-	 * removed from the main method.
-	 *
-	 * TODO Delete this after reading.
-	 */
 }
