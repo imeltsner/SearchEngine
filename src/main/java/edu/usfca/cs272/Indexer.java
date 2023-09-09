99e1dacabd9a93ad1404a3676f2409ff40eea212
isaac meltsner <itmeltsner@dons.usfca.edu>
2023-09-08 21:06:53 -0700

    added utility methods

--- src/main/java/edu/usfca/cs272/Indexer.java
+++ src/main/java/edu/usfca/cs272/Indexer.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class Indexer {
-
+    /**
+     * Stores filenames and wordcounts
+     */
     private TreeMap<String, Integer> wordCounts;
+    /**
+     * Stores words, filenames, and positions in file
+     */
     private TreeMap<String, TreeMap<String, ArrayList<Integer>>> invertedIndex; 
 
     public Indexer() {
@@ -17,7 +22,15 @@ public class Indexer {
         return wordCounts;
     }
 
-    public TreeMap<String, TreeMap<String, ArrayList<Integer>>> getIndex() {
+    public TreeMap<String, TreeMap<String, ArrayList<Integer>>> getOuter() {
         return invertedIndex;
+    }
+
+    public TreeMap<String, ArrayList<Integer>> getInner(String key) {
+        return invertedIndex.get(key);
+    }
+
+    public boolean containsWord(String word) {
+        return invertedIndex.get(word) == null;
     }
 }
