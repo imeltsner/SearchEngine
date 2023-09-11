d41853c1f00ffa977ec4cf0e0310e934e93b8ce1
isaac meltsner <itmeltsner@dons.usfca.edu>
2023-09-10 18:35:28 -0700

    added fuctionality to add data to inverted index and docs

--- src/main/java/edu/usfca/cs272/Indexer.java
+++ src/main/java/edu/usfca/cs272/Indexer.java
@@ -8,6 +8,7 @@ public class Indexer {
      * Stores filenames and wordcounts
      */
     private TreeMap<String, Integer> wordCounts;
+
     /**
      * Stores words, filenames, and positions in file
      */
@@ -18,19 +19,60 @@ public class Indexer {
         invertedIndex = new TreeMap<>();
     }
 
+    
+    /**
+     * Getter function for retrieving TreeMap containing
+     * file names and word counts
+     * @return the Treemap containing file names and word counts
+     */
     public TreeMap<String, Integer> getCounts() {
         return wordCounts;
     }
 
-    public TreeMap<String, TreeMap<String, ArrayList<Integer>>> getOuter() {
+    
+    /**
+     * Getter function for retrieving the inverted index data structure
+     * @return the inverted index
+     */
+    public TreeMap<String, TreeMap<String, ArrayList<Integer>>> getWordMap() {
         return invertedIndex;
     }
 
-    public TreeMap<String, ArrayList<Integer>> getInner(String key) {
-        return invertedIndex.get(key);
+    /**
+     * Getter function for retrieving TreeMap associated with a word
+     * containing file names and word positions
+     * @param word key associated with inner TreeMap
+     * @return inner TreeMap of inverted index
+     */
+    public TreeMap<String, ArrayList<Integer>> getFileMap(String word) {
+        return invertedIndex.get(word);
     }
 
-    public boolean containsWord(String word) {
-        return invertedIndex.get(word) == null;
+    /**
+     * Adds position data to the inverted index
+     * @param word the word in the file
+     * @param fileName the name of the file
+     * @param position the words position in the file
+     * 
+     * @see #getFileMap(String)
+     */
+    public void putPosition(String word, String fileName, int position) {
+        getFileMap(word).get(fileName).add(position);
+    }
+
+    /**
+     * Adds words, filenames, and word position
+     * to the inverted index
+     * @param word the word to add
+     * @param fileName the file to add
+     * @param position the position of the word
+     * 
+     * @see #getFileMap(String)
+     * @see #putPosition(String, String, int)
+     */
+    public void putData(String word, String fileName, int position) {
+        invertedIndex.putIfAbsent(word, new TreeMap<>());
+        getFileMap(word).putIfAbsent(fileName, new ArrayList<>());
+        putPosition(word, fileName, position);
     }
 }
