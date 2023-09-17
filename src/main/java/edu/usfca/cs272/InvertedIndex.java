85cebdd6baf4c17005d58649fe6d44534ce6a9f3
isaac meltsner <itmeltsner@dons.usfca.edu>
2023-09-16 18:21:56 -0700

    added more utility methods an toString() method

--- src/main/java/edu/usfca/cs272/InvertedIndex.java
+++ src/main/java/edu/usfca/cs272/InvertedIndex.java
@@ -27,29 +27,70 @@ public class InvertedIndex {
         wordCounts = new TreeMap<>();
         invertedIndex = new TreeMap<>();
     }
-
     
     /**
      * Getter function for retrieving TreeMap containing
      * file names and word counts
      * @return the Treemap containing file names and word counts
      */
-    public TreeMap<String, Integer> getCounts() {
+    public TreeMap<String, Integer> getWordCounts() {
         return wordCounts;
     }
 
+    /**
+     * Gets a word count from the wordCounts map
+     * @param file the file associated with the count
+     * @return the number of words in a file or 0 if file not found
+     * 
+     * @see #wordCountHas(String)
+     */
+    public int getCount(String file) {
+        return wordCountHas(file) ? wordCounts.get(file) : 0;
+    }
+
+    /**
+     * Checks if wordCount map contains a given file name
+     * @param file the file name to check for
+     * @return true if file name is in map false otherwise
+     */
+    public boolean wordCountHas(String file) {
+        return wordCounts.containsKey(file);
+    }
     
     /**
      * Getter function for retrieving the inverted index data structure
      * @return the inverted index
      */
-    public TreeMap<String, TreeMap<String, TreeSet<Integer>>> getWordMap() {
+    public TreeMap<String, TreeMap<String, TreeSet<Integer>>> getInvertedIndex() {
         return invertedIndex;
     }
 
     /**
-     * Getter function for retrieving TreeMap associated with a word
-     * containing file names and word positions
+     * Checks if a given word is stored in the inverted index
+     * @param word the word to check
+     * @return true if word is in inverted index false otherwise
+     */
+    public boolean hasWord(String word) {
+        return invertedIndex.containsKey(word);
+    }
+
+    /**
+     * Checks if a given word was found in a given file in the inverted index
+     * @param word the word to check
+     * @param file the file to check
+     * @return true if the word was found in the file, false if the word
+     * is not in the inverted index or not found in the file
+     * 
+     * @see #hasWord(String)
+     * @see #getFileMap(String)
+     */
+    public boolean inFile(String word, String file) {
+        return hasWord(word) ? getFileMap(word).containsKey(file) : false;
+    }
+
+    /**
+     * Getter function for retrieving TreeMap with all 
+     * file names word appeared in and positions in the file word appeared
      * @param word key associated with inner TreeMap
      * @return inner TreeMap of inverted index
      */
@@ -58,7 +99,7 @@ public class InvertedIndex {
     }
 
     /**
-     * Adds position data to the inverted index
+     * Adds the position a word was found to the inverted index
      * @param word the word in the file
      * @param fileName the name of the file
      * @param position the words position in the file
@@ -85,6 +126,11 @@ public class InvertedIndex {
         putPosition(word, fileName, position);
     }
     
+    @Override
+    public String toString() {
+        return invertedIndex.toString();
+    }
+
     /*
      * TODO 
      * More generally useful data structure methods here
