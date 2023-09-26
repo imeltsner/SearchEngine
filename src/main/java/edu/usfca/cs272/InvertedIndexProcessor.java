40e67660922110c4096d7afd4925004280db87ae
isaac meltsner <itmeltsner@dons.usfca.edu>
2023-09-25 21:58:45 -0700

    refactored process file

--- src/main/java/edu/usfca/cs272/InvertedIndexProcessor.java
+++ src/main/java/edu/usfca/cs272/InvertedIndexProcessor.java
@@ -1,11 +1,18 @@
 package edu.usfca.cs272;
 
+import static opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM.ENGLISH;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+
+import opennlp.tools.stemmer.Stemmer;
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
 
 /**
  * Class responsible for iterating through files and directories
@@ -14,27 +21,74 @@ import java.util.ArrayList;
  * @author Isaac Meltsner
  */
 public class InvertedIndexProcessor {
+	/** Regular expression that matches any whitespace. **/
+	public static final Pattern SPLIT_REGEX = Pattern.compile("(?U)\\p{Space}+");
+
+	/** Regular expression that matches non-alphabetic characters. **/
+	public static final Pattern CLEAN_REGEX = Pattern.compile("(?U)[^\\p{Alpha}\\p{Space}]+");
+
 	/**
-	 * Reads a file, cleans each word and stems words
-	 * Adds word counts and word positions in files to Indexer 
-	 * @param inPath path of the file
+	 * Cleans the text by removing any non-alphabetic characters (e.g. non-letters
+	 * like digits, punctuation, symbols, and diacritical marks like the umlaut) and
+	 * converting the remaining characters to lowercase.
+	 *
+	 * @param text the text to clean
+	 * @return cleaned text
+	 */
+	public static String clean(String text) {
+		String cleaned = Normalizer.normalize(text, Normalizer.Form.NFD);
+		cleaned = CLEAN_REGEX.matcher(cleaned).replaceAll("");
+		return cleaned.toLowerCase();
+	}
+
+	/**
+	 * Splits the supplied text by whitespaces.
+	 *
+	 * @param text the text to split
+	 * @return an array of {@link String} objects
+	 */
+	public static String[] split(String text) {
+		return text.isBlank() ? new String[0] : SPLIT_REGEX.split(text.strip());
+	}
+
+	/**
+	 * Parses the text into an array of clean words.
+	 *
+	 * @param text the text to clean and split
+	 * @return an array of {@link String} objects
+	 *
+	 * @see #clean(String)
+	 * @see #parse(String)
+	 */
+	public static String[] parse(String text) {
+		return split(clean(text));
+	}
+
+	/**
+	 * Reads a file, cleans and stems each word
+	 * Adds word counts to map and word positions in files to inverted index 
+	 * @param path path of the file
 	 * @param index the Indexer object
 	 * @throws IOException if an IOException occurs
 	 * 
 	 */
-	public static void processFile(Path inPath, InvertedIndex index) throws IOException {
-		/*
-		 * TODO Copy/paste some logic from filestemmer into here
-		 * so you can go from stem to index without ever creating a list
-		 */
-		ArrayList<String> stems = FileStemmer.listStems(inPath);
+	public static void processFile(Path path, InvertedIndex index) throws IOException {
+		Stemmer stemmer = new SnowballStemmer(ENGLISH);
+		String sPath = path.toString();
+		int count = 0;
 
-		if (stems.size() != 0) {
-			index.getWordCounts().put(inPath.toString(), stems.size());
-		}
+		try (BufferedReader reader = Files.newBufferedReader(path)) {
+			while (reader.ready()) {
+				String[] words = parse(reader.readLine());
+				for (String word: words) {
+					index.putData(stemmer.stem(word).toString(), sPath, count + 1);
+					count++;
+				}
+			}
 
-		for (int i = 0; i < stems.size(); i++) {
-			index.putData(stems.get(i), inPath.toString(), i+1);
+			if (count != 0) {
+				index.putCount(sPath, count);
+			}
 		}
 	}
 
@@ -42,23 +96,23 @@ public class InvertedIndexProcessor {
 	 * Recursively iterates through a directory
 	 * checks if files are text files
 	 * adds contents of text files to inverted index
-	 * @param inPath path of directory
+	 * @param path path of directory
 	 * @param index the Indexer object
 	 * @throws IOException if IO error occurs
 	 * @throws NotDirectoryException if given path is not a directory
      * 
      * @see #processFile(Path, InvertedIndex)
 	 */
-	public static void processDir(Path inPath, InvertedIndex index) throws IOException, NotDirectoryException {
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(inPath);) {
+	public static void processDir(Path path, InvertedIndex index) throws IOException, NotDirectoryException {
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(path);) {
 			var iterator = stream.iterator();
 			while (iterator.hasNext()) {
-				Path path = iterator.next();
-				if (Files.isDirectory(path)) {
-					processDir(path, index);
+				Path newPath = iterator.next();
+				if (Files.isDirectory(newPath)) {
+					processDir(newPath, index);
 				}
-				else if (isTextFile(path)) {
-					processFile(path, index);
+				else if (isTextFile(newPath)) {
+					processFile(newPath, index);
 				}
 			}
 		} 
