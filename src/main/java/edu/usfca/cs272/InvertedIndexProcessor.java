e03eb08b3c30fa59c59ac3cdb78480c89f3d0469
isaac meltsner <itmeltsner@dons.usfca.edu>
2023-10-31 13:52:34 -0700

    added overloaded processFile

--- src/main/java/edu/usfca/cs272/InvertedIndexProcessor.java
+++ src/main/java/edu/usfca/cs272/InvertedIndexProcessor.java
@@ -44,6 +44,28 @@ public class InvertedIndexProcessor {
 		}
 	}
 
+	/**
+	 * Reads a file, cleans and stems each word
+	 * Adds word counts to map and word positions in files to inverted index 
+	 * @param path path of the file
+	 * @param index the Indexer object
+	 * @param stemmer the stemmer to use
+	 * @throws IOException if an IOException occurs
+	 */
+	public static void processFile(Path path, InvertedIndex index, Stemmer stemmer) throws IOException {
+		int count = 0;
+
+		try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+			while (reader.ready()) {
+				String[] words = FileStemmer.parse(reader.readLine());
+				for (String word: words) {
+					index.addData(stemmer.stem(word).toString(), path.toString(), count + 1);
+					count++;
+				}
+			}
+		}
+	}
+
     /**
 	 * Recursively iterates through a directory
 	 * checks if files are text files
