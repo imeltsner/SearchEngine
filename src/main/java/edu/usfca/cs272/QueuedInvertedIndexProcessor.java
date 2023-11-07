9113bbb45d3c915849ea5cf5a5a63eb48caa09fa
isaac meltsner <itmeltsner@dons.usfca.edu>
2023-11-07 13:43:38 -0800

    use addAll

--- src/main/java/edu/usfca/cs272/QueuedInvertedIndexProcessor.java
+++ src/main/java/edu/usfca/cs272/QueuedInvertedIndexProcessor.java
@@ -3,6 +3,7 @@ package edu.usfca.cs272;
 import static opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM.ENGLISH;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.NotDirectoryException;
@@ -17,97 +18,101 @@ import opennlp.tools.stemmer.snowball.SnowballStemmer;
  * @author Isaac Meltsner
  */
 public class QueuedInvertedIndexProcessor {
-	
-	// TODO These need a thread-safe index
-	
-	 /**
-	 * Recursively iterates through a directory checks if files are text files
-	 * adds contents of text files to inverted index
-	 * @param path path of directory
-	 * @param index the Indexer object
-	 * @param queue the work queue to use
-	 * @throws IOException if IO error occurs
-	 * @throws NotDirectoryException if given path is not a directory
-	 */
-	public static void processDir(Path path, ThreadSafeInvertedIndex index, WorkQueue queue) throws IOException, NotDirectoryException {
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(path);) {
+     /**
+     * Recursively iterates through a directory checks if files are text files
+     * adds contents of text files to inverted index
+     * @param path path of directory
+     * @param index the Indexer object
+     * @param queue the work queue to use
+     * @throws IOException if IO error occurs
+     * @throws NotDirectoryException if given path is not a directory
+     */
+    public static void processDir(Path path, ThreadSafeInvertedIndex index, WorkQueue queue) throws IOException, NotDirectoryException {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path);) {
 
-			var iterator = stream.iterator();
+            var iterator = stream.iterator();
 
-			while (iterator.hasNext()) {
+            while (iterator.hasNext()) {
 
-				Path newPath = iterator.next();
-				
-				if (Files.isDirectory(newPath)) {
-					processDir(newPath, index, queue);
-				}
-				else if (InvertedIndexProcessor.isTextFile(newPath)) {
-					Task task = new Task(newPath, index);
-					queue.execute(task);
-				}
-			}
-		} 
-	}
+                Path newPath = iterator.next();
+                
+                if (Files.isDirectory(newPath)) {
+                    processDir(newPath, index, queue);
+                }
+                else if (InvertedIndexProcessor.isTextFile(newPath)) {
+                    Task task = new Task(newPath, index);
+                    queue.execute(task);
+                }
+            }
+        } 
+    }
 
-	/**
-	 * Processes path according to path type
-	 * @param path the path to process
-	 * @param index the inverted index
-	 * @param queue the work queue to use
-	 * @throws IOException if IOError occurs
-	 * @throws NullPointerException if null pointer is found
-	 */
-	public static void process(Path path, ThreadSafeInvertedIndex index, WorkQueue queue) throws IOException, NullPointerException {
-		if (Files.isDirectory(path)) {
-			processDir(path, index, queue);
-		}
-		else {
-			Task task = new Task(path, index);
-			queue.execute(task);
-		}
-	}
+    /**
+     * Processes path according to path type
+     * @param path the path to process
+     * @param index the inverted index
+     * @param queue the work queue to use
+     * @throws IOException if IOError occurs
+     * @throws NullPointerException if null pointer is found
+     */
+    public static void process(Path path, ThreadSafeInvertedIndex index, WorkQueue queue) throws IOException, NullPointerException {
+        if (Files.isDirectory(path)) {
+            processDir(path, index, queue);
+        }
+        else {
+            Task task = new Task(path, index);
+            queue.execute(task);
+        }
+    }
 
-	/** Processes a single file */
-	private static class Task implements Runnable {
-		/** The path of the file to process */
-		private final Path path;
+    /** Processes a single file */
+    private static class Task implements Runnable {
+        /** The path of the file to process */
+        private final Path path;
 
-		/** The inverted index to use */
-		private final InvertedIndex index;
+        /** The main thread inverted index to use */
+        private final ThreadSafeInvertedIndex index;
 
-		/** The stemmer to use */
-		private final Stemmer stemmer;
+        /** The local index */
+        private final InvertedIndex local;
 
-		/**
-		 * Class constructor
-		 * @param path the path of a file
-		 * @param index the inverted index to use
-		 */
-		private Task(Path path, InvertedIndex index) {
-			this.path = path;
-			this.index = index;
-			this.stemmer = new SnowballStemmer(ENGLISH);
-		}
+        /** The stemmer to use */
+        private final Stemmer stemmer;
 
-		@Override
-		public void run() {
-			try {
-				InvertedIndexProcessor.processFile(path, index, stemmer);
-				
-				/*
-				 	TODO 
-					1. Create local data inside of run
-					2. Added to the local data not the shared data
-					3. Some kind of addAll to combine together local and shared data in a safe way
-					
-					InvertedIndex local = ...
-					processFile(path, local, stemmer);
-					index.addAll(local)
-				 */
-			} 
-			catch (IOException e) {
-				System.out.println("IO ERROR"); // TODO throw UncheckedIOExecption(e);
-			}
-		}
-	}
+        /**
+         * Class constructor
+         * @param path the path of a file
+         * @param index the inverted index to use
+         */
+        private Task(Path path, ThreadSafeInvertedIndex index) {
+            this.path = path;
+            this.index = index;
+            this.stemmer = new SnowballStemmer(ENGLISH);
+            this.local = new InvertedIndex();
+        }
+
+        @Override
+        public void run() {
+            try {
+                InvertedIndexProcessor.processFile(path, local, stemmer);
+                synchronized (index) {
+                    index.addAll(local);
+                }
+
+                
+                /*
+                    1. Create local data inside of run
+                    2. Added to the local data not the shared data
+                    3. Some kind of addAll to combine together local and shared data in a safe way
+                    
+                    InvertedIndex local = ...
+                    processFile(path, local, stemmer);
+                    index.addAll(local)
+                 */
+            } 
+            catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+    }
 }
