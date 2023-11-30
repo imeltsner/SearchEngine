554c95661f28526feda08957846d0cec0668c986
isaac meltsner <itmeltsner@dons.usfca.edu>
2023-11-30 14:44:05 -0800

    updated class

--- src/main/java/edu/usfca/cs272/WebCrawler.java
+++ src/main/java/edu/usfca/cs272/WebCrawler.java
@@ -1,7 +1,15 @@
 package edu.usfca.cs272;
 
+import static opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM.ENGLISH;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+
+import opennlp.tools.stemmer.Stemmer;
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
 
 public class WebCrawler {
 	/** The total number of links to crawl */
@@ -13,8 +21,8 @@ public class WebCrawler {
 	/** The work queue to use */
 	private final WorkQueue queue;
 
-	/** A set of all urls that have been crawled */
-	private final Set<String> seenURLs;
+	/** A list of all the urls to crawl */
+	private final List<URL> URLs;
 
 	/** The inverted index to use */
 	private final ThreadSafeInvertedIndex index;
@@ -23,7 +31,7 @@ public class WebCrawler {
 		this.seed = LinkFinder.removeFragment(seed);
 		this.maxLinks = maxLinks > 0 ? maxLinks : 1;
 		this.queue = queue;
-		this.seenURLs = new HashSet<>();
+		this.URLs = new ArrayList<>();
 		this.index = index;
 	}
 
@@ -31,13 +39,63 @@ public class WebCrawler {
 		this.seed = LinkFinder.removeFragment(seed);
 		this.maxLinks = 1;
 		this.queue = queue;
-		this.seenURLs = new HashSet<>();
+		this.URLs = new ArrayList<>();
 		this.index = index;
 	}
 
-	public void crawlLinks() {
+	public void crawlLinks() throws MalformedURLException, NullPointerException {
+		URL seedURL = new URL(seed);
+		if (URLs.size() < maxLinks) {
+			URLs.add(seedURL);
+		}
+
 		String html = HtmlFetcher.fetch(seed, 3);
+		HashSet<URL> uniqueLinks = LinkFinder.uniqueUrls(new URL(seed), html);
+		URLs.addAll(uniqueLinks);
+
 		String cleanHtml = HtmlCleaner.stripHtml(html);
 		InvertedIndexProcessor.processString(cleanHtml, index, seed, 0);
+
+		if (maxLinks > 1 && URLs.size() > 1) {
+			for (int i = 1; i < URLs.size(); i++) {
+				Task task = new Task(URLs.get(i), index);
+				queue.execute(task);
+			}
+		}
+	}
+
+	private class Task implements Runnable {
+		/** The url to parse */
+		private final URL url;
+
+		/** The main thread inverted index to use */
+		private final ThreadSafeInvertedIndex index;
+
+		/** The local index */
+		private final InvertedIndex local;
+
+		/** The stemmer to use */
+		private final Stemmer stemmer;
+
+		public Task(URL url, ThreadSafeInvertedIndex index) {
+			this.url = url;
+			this.index = index;
+			this.local = new InvertedIndex();
+			this.stemmer = new SnowballStemmer(ENGLISH);
+		}
+
+		@Override
+		public void run() {
+			String html = HtmlFetcher.fetch(url);
+			HashSet<URL> uniqueLinks = LinkFinder.uniqueUrls(url, html);
+
+			synchronized (URLs) {
+				URLs.addAll(uniqueLinks);
+			}
+
+			String cleanHtml = HtmlCleaner.stripHtml(html);
+			InvertedIndexProcessor.processString(cleanHtml, local, url.toString(), 0, stemmer);
+			index.addAll(local);
+		}
 	}
 }
